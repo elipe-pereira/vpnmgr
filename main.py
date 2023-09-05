f2c0136edf30e43c75a2b4a4682a8bb550023de5
Eli <eli@hanokh.com.br>
2023-09-05 04:49:33 -0300

    Adicionado arquivo main.py ao projeto.

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# coding: utf-8
+import os
+import sys
+
+
+class Main:
+    def __int__(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = Main()
+    app.run()
