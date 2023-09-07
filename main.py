b110b3162b6180ccc8e7ca1ca5c099ff28a36d4a
Eli <eli@hanokh.com.br>
2023-09-06 21:04:44 -0300

    update

--- main.py
+++ main.py
@@ -2,14 +2,19 @@
 # coding: utf-8
 import os
 import sys
+from app.scripts.vpn_manager import VPNManager
 
 
 class Main:
-    def __int__(self):
-        pass
+    def __init__(self):
+        self.vpn_manager = None
+        self.base_path = os.path.dirname(os.path.realpath(__file__))
+        if self.base_path not in sys.path:
+            sys.path.append(self.base_path)
 
     def run(self):
-        pass
+        self.vpn_manager = VPNManager()
+        self.vpn_manager.exec()
 
 
 if __name__ == "__main__":
